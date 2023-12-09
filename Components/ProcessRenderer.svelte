c628bdba9e13010a170ff3876d213c2c4afa3a95
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-09 22:03:38 +0100

    Begin working on Desktop

---
+++ Components/ProcessRenderer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { processes } from "$ts/stores/apps";
+  import { App } from "$types/app";
+
+  let procs: [string, App | "disposed"][] = [];
+
+  processes.subscribe((v) => (procs = Object.entries(v)));
+</script>
+
+<div class="process-renderer">
+  {#each procs as [pid, proc]}
+    {#if proc == "disposed"}
+      <div class="disposed pid-{pid}"></div>
+    {:else}
+      <div class="window">{pid}</div>
+    {/if}
+  {/each}
+</div>
