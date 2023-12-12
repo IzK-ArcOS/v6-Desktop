81e6658b963a3dcd7b52acdc0f21004dda2be740
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-12 17:42:42 +0100

    Definitively fix DOM inheritance bug

--- Components/ProcessRenderer.svelte
+++ Components/ProcessRenderer.svelte
@@ -1,29 +1,14 @@
 <script lang="ts">
   import { closedPids, processes } from "$ts/stores/apps";
-  import { sleep } from "$ts/util";
-  import { App } from "$types/app";
   import Window from "./ProcessRenderer/Window.svelte";
-
-  let procs: /* [string, App | "disposed"] */ string[] = [];
-
-  processes.subscribe(async (v) => {
-    procs = [];
-    await sleep(0); // Wait for animationFrame (PS 1 of 2)
-    procs = Object.keys(v);
-  });
 </script>
 
 <div class="process-renderer">
-  {#each procs as pid}
-    <!-- {console.log(proc)} -->
-    {#if $processes[pid] == "disposed"}
+  {#each [...$processes] as [pid, proc]}
+    {#if proc == "disposed"}
       <div class="disposed pid-{pid}"></div>
     {:else}
-      <Window
-        proc={$processes[pid]}
-        pid={parseInt(pid)}
-        closing={$closedPids.includes(parseInt(pid))}
-      />
+      <Window {proc} {pid} closing={$closedPids.includes(pid)} />
     {/if}
   {/each}
 </div>
