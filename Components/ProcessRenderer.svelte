0d6a11d357f6fbc46a79789f5157ffdde45932c0
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-15 18:06:20 +0100

    Update process renderer

--- Components/ProcessRenderer.svelte
+++ Components/ProcessRenderer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { closedPids, processes } from "$ts/stores/apps";
+  import { ClosedPids, processes } from "$ts/stores/apps";
   import Window from "./ProcessRenderer/Window.svelte";
 </script>
 
@@ -8,7 +8,7 @@
     {#if proc == "disposed"}
       <div class="disposed pid-{pid}" />
     {:else}
-      <Window {proc} {pid} closing={$closedPids.includes(pid)} />
+      <Window {proc} {pid} closing={$ClosedPids.includes(pid)} />
     {/if}
   {/each}
 </div>
--- Components/ProcessRenderer/Window/Titlebar.svelte
+++ Components/ProcessRenderer/Window/Titlebar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { ARCOS_MODE } from "$ts/metadata";
-  import { closedPids } from "$ts/stores/apps";
+  import { ClosedPids } from "$ts/stores/apps";
   import { App } from "$types/app";
   import { ReadableStore } from "$types/writable";
   import Controls from "./Titlebar/Controls.svelte";
@@ -9,7 +9,7 @@
   export let pid: number;
 
   function maximize() {
-    if ($closedPids.includes(pid)) return; // Closing: don't do anything
+    if ($ClosedPids.includes(pid)) return; // Closing: don't do anything
 
     $app.state.maximized = !$app.state.maximized;
   }
