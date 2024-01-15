b0feebf28b1a95727a5e5aa1dccaf194619398bb
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-15 22:25:55 +0100

    Add window tabs

---
+++ Components/ProcessRenderer/Window/Tabs.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let tabs: string[] = [];
+  export let current = "";
+
+  onMount(() => {
+    current = tabs.length ? tabs[0] : "";
+
+    console.log(current);
+  });
+</script>
+
+<div class="tab-interface">
+  <div class="tabs">
+    {#each tabs as tab}
+      <button
+        class="tab"
+        class:suggested={current == tab}
+        on:click={() => (current = tab)}
+      >
+        {tab}
+      </button>
+    {/each}
+    <div class="right">
+      <slot name="right" />
+    </div>
+  </div>
+  <slot name="renderer" />
+</div>
---
+++ Components/ProcessRenderer/Window/Tabs/Tab.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let current: string;
+  export let selector: string;
+</script>
+
+<div class="tab" class:visible={current == selector}>
+  <slot />
+</div>
