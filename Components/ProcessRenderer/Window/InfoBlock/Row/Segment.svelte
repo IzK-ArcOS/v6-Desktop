76beea019085770f9025f4421cd6775ddc3e2029
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-18 15:37:32 +0100

    Make InfoBlocks a global component

---
+++ Components/ProcessRenderer/Window/InfoBlock/Row/Segment.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Nullable } from "$types/common";
+
+  export let title: string;
+  export let help: Nullable<() => any> = null;
+  export let right = false;
+  export let className = "";
+</script>
+
+<div class="segment {className}" class:right>
+  <p class="title">
+    {title}
+    {#if help}
+      <button class="help material-icons-round" on:click={help}>help</button>
+    {/if}
+  </p>
+  <p class="value">
+    <slot />
+  </p>
+</div>
